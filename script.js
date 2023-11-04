fc4ddfe1ebcb52921f1d7afff7f8ad4df93ba0b8
jeppeklh <jeppeklh@hotmail.com>
2023-11-04 14:27:50 +0100

    Added buttons to change grid size

--- script.js
+++ script.js
@@ -3,20 +3,43 @@ const gridContainer = document.querySelector(".grid-container");
 //number of rows and columns
 let gridSize = 16;
 
-function makeGrid(color) {
+function makeGrid(gridNumber, elementSize) {
+  gridSize = gridNumber;
+
   for (i = 0; i < gridSize ** 2; i++) {
     const block = document.createElement("div");
     block.classList.add("grid-item");
     block.style.cssText =
-      "border: 1px solid black; background-color: " + color + "; height: 25px; width: 25px;";
+      "border-left: 1px solid black; border-top: 1px solid black; background-color: white; height: " + elementSize + "px; width: " + elementSize + "px;";
+    gridContainer.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
     gridContainer.appendChild(block);
   }
 }
 
-makeGrid("white");
+function smallGrid() {
+  changeGridSize(20, 25);
+}
+
+function mediumGrid() {
+  changeGridSize(40, 12.1);
+}
+
+function bigGrid() {
+  changeGridSize(80, 5.65);
+}
+
+
+
+//initial Grid
+makeGrid(20, 25);
 
 const yellowButton = document.querySelector(".yellow-button");
 const clearButton = document.querySelector(".clear-button");
+const bigButton = document.querySelector(".big-button");
+const mediumButton = document.querySelector(".medium-button");
+const smallButton = document.querySelector(".small-button");
+
 
 function changeBgToYellow() {
   const gridItem = document.querySelectorAll(".grid-item");
@@ -33,3 +56,18 @@ function clearGrid() {
 }
 yellowButton.addEventListener("click", changeBgToYellow);
 clearButton.addEventListener("click", clearGrid);
+bigButton.addEventListener("click", bigGrid);
+mediumButton.addEventListener("click", mediumGrid);
+smallButton.addEventListener("click", smallGrid);
+
+function changeGridSize(newSize, elementSize) {
+  //delete initial grid
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.lastChild);
+  }
+  //make new grid
+  makeGrid(newSize, elementSize);
+}
+
+
+
